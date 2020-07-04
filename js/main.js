3717111e72d7a3b4de2b9369cf476edba584f7f2
omareyad <huckeroo07@gmail.com>
2020-07-04 06:09:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+/*  Type H1 In Home Page Function */
+let typeEffect = (element, speed) => {
+    let texth1 = element.innerText; //OmarEyad;
+    element.innerText = '';
+    let i = 0;
+    let timer = setInterval(function () {
+        if (i < texth1.length) {
+            element.append(texth1.charAt(i));
+            i++;
+        }
+        else {
+            document.querySelector('.arrowDown');
+            let btnHtml = `<div class="arrow-down">
+        <div class="right"></div>
+          <div class="left"></div>
+      </div>`;
+            document.querySelector('.arrowDown').innerHTML = btnHtml;
+            clearInterval(timer);
+        }
+    }, speed);
+
+}
+
+//call Method Eeffect :
+
+let heading1 = document.getElementById('heading1');
+typeEffect(heading1, 150);
+
+/* Tabs Method */
+
+let TabsUl = Array.from(document.querySelectorAll('.tabsMenu .tabItem')); //Array of Items
+let Tabs = Array.from(document.querySelectorAll('.tabs .tab'));
+console.log(TabsUl);
+console.log(Tabs);
+
+
+const TabsBtn = (index) =>{
+
+
+TabsUl.forEach(node =>{
+        node.classList.remove('activeTabs');
+    });
+Tabs.forEach(node =>{
+    node.classList.remove('showTab');
+});
+TabsUl[index].classList.add('activeTabs');
+Tabs[index].classList.add('showTab');
+}
+
+
+for(var i = 0 ; i<TabsUl.length ; i++)
+{
+TabsUl[i].setAttribute('data-index', i);
+}
+
+
+
+for(var i = 0 ; i<TabsUl.length ; i++){
+    TabsUl[i].onclick = function()
+    {
+  let index = parseInt(this.getAttribute('data-index'));
+  console.log(index);
+ TabsBtn(index);
+
+    };
+  }
+
+/* Arrow UP Show Hide */
+  let ArrowUP = document.querySelector('.arrowup');
+  console.log(ArrowUP);
+
+  window.onscroll = ()=>{
+    (window.pageYOffset > 740)? ArrowUP.classList.add('showTab'):ArrowUP.classList.remove('showTab');
+  }
+
+  /* Arrow UP Scroll */
+
+  ArrowUP.onclick = () =>{
+    window.scrollTo({top:0,behavior:'smooth'});
+  }
+
+  /*Arrow Down */
+let ArrowDown = document.querySelector('.arrowDown');
+let mySkills = document.querySelector('.mySkills-tabs');
+let rect = mySkills.getBoundingClientRect().top;
+ArrowDown.onclick = ()=>{
+    window.scrollTo({top:rect,behavior:'smooth'});
+}
+
+let scrollNav = (index) =>{
+  window.scrollTo({top:rect,behavior:'smooth'});
+  TabsBtn(index);
+  console.log(index);
+}
+let friendSection = document.querySelector('.Be-friends').getBoundingClientRect().top;
+let scrollfriend = () =>{
+  console.log(friendSection);
+window.scrollTo({top:friendSection,behavior:'smooth'});
+}
